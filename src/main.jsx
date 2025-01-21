cccb8501220d9b3cb90ae9a6c539c0c13f9f2a3f
mihir59159 <mihirpatel4529@gamil.com>
2025-01-22 01:31:27 +0530

    reolve main Authcontext path

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { ContextProvider } from './utils/AuthContext.jsx'
+import { ContextProvider } from './utils/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
